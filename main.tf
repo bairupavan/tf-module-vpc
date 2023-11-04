2d2fe2b52ec4f11f9c79428eedb5f5640462c807
pavanbairu <pavan.bairu1@gmail.com>
2023-11-04 16:36:08 +0530

    adding tags for vpc

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ resource "aws_vpc" "vpc" {
   cidr_block = var.cidr_block
   enable_dns_support = true
   enable_dns_hostnames = true
-  tags = merge(local.tags, Name = "${var.env}-vpc")
+  tags = merge(local.tags, { Name = "${var.env}-vpc" })
 }
