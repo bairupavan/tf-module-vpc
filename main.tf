3816752aa830e1c1b469dc6973ab1b55a8d27d0e
pavanbairu <pavan.bairu1@gmail.com>
2023-11-04 19:13:26 +0530

    creating internet gateway, elastic ip and nat gateway

--- main.tf
+++ main.tf
@@ -16,3 +16,27 @@ module "subnet" {
   tags = var.tags
   env = var.env
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = merge(var.tags, { Name = "${var.env}-igw" })
+}
+
+resource "aws_eip" "elastic_ip" {
+  count = length(var.subnets["public"].cidr_block)
+  vpc = true
+  tags = merge(var.tags, { Name = "${var.env}-eip" })
+}
+
+resource "aws_nat_gateway" "example" {
+  count = length(var.subnets["public"].cidr_block)
+  allocation_id = aws_eip.elastic_ip[count.index].id
+  subnet_id     = module.subnet["public"].subnet_ids[count.index]
+
+  tags = merge(var.tags, { Name = "${var.env}-ngw" })
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
+}
