2d4e1051196755ddeb79af6bd67e809e5c1e990e
pavanbairu <pavan.bairu1@gmail.com>
2023-11-05 14:19:06 +0530

    fixing subnet

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  all_private_subnet_cidrs = concat(module.subnet["app"].route_table_ids, module.subnet["web"].route_table_ids,
-    module.subnet["db"].route_table_ids)
+  all_private_subnet_cidrs = concat(module.subnets["app"].route_table_ids, module.subnets["web"].route_table_ids,
+    module.subnets["db"].route_table_ids)
 }
