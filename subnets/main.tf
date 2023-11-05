bc49531550ff23457f0cf69aa39980dd0ac38063
pavanbairu <pavan.bairu1@gmail.com>
2023-11-05 09:59:00 +0530

    added tags for route tables

--- subnets/main.tf
+++ subnets/main.tf
@@ -10,6 +10,8 @@ resource "aws_subnet" "subnet" {                  # creating the resource subnet
 resource "aws_route_table" "rt" {
   count  = length(var.cidr_block) # there are 2 cidr_blocks for each route so 2 routes created foreach server types
   vpc_id = var.vpc_id             # sending the vpc_id to create route under that vpc
+
+  tags = merge(var.tags, { Name = "${var.env}-${var.name}-rt-${count.index + 1}" }) # merging the tags
 }
 
 resource "aws_route_table_association" "rt_association" {
